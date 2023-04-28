5bac002dc49b3b74e34d4f0c55708bc97c330bcd
kingswithkings <kingsuthanaidogu@gmail.com>
2023-04-28 11:44:42 +0100

    Add project files.

---
+++ comparison operator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace comparison_operator_in_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int myage = 25;
+            int votingage = 18;
+
+            if (myage >= votingage)
+            {
+                Console.WriteLine("old enough to vote");
+            }
+            else
+            {
+                Console.WriteLine("not old enough to vote");
+            }
+        }
+    }
+}
